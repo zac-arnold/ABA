da9e7bdcfff68d1d74caa00a26cc287c6d8a1758
Ariel <33848430+arielwc@users.noreply.github.com>
2020-07-16 14:54:00 +1200

    creates form for name and amount in Income component

--- client/components/Income.jsx
+++ client/components/Income.jsx
@@ -3,22 +3,27 @@ import { connect } from 'react-redux'
 
 class Income extends React.Component {
   state = {
-    incomes: [{name:"" , amount: "", occurrance:"weekly"}]
+    incomes: [{ name: '', amount: 0, occurrance: 'weekly' }]
   }
 
   render () {
     return (
       <div>
-        Hello
+        <form>
+          <label htmlFor="name">Name </label>
+          <input type="text" value="" />
+
+          <label htmlFor="amount">Amount </label>
+          <input type="text" value="" />
+          <button type="submit" value="submit">Submit</button>
+        </form>
       </div>
     )
   }
 }
 
 const mapStateToProps = (state) => ({
-  
+
 })
-
-
 
 export default connect(mapStateToProps)(Income)
