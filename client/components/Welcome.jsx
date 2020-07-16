fd97b43ec19b6d0998c72d91598d6500b9575113
Ariel <33848430+arielwc@users.noreply.github.com>
2020-07-16 14:19:05 +1200

    creates SideBar component

--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -2,20 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Welcome = () => {
-<<<<<<< HEAD
   return (
     <div id='welcome-page'>
+      <Title />
       <div id='welcome-create-budget-button'><Link to='/bugdet'>Create a budget</Link></div>
     </div>
   )
-=======
-    return(
-        <div id='welcome-page'>
-            <Title />
-            <div id='welcome-create-budget-button'><Link to='/bugdet'>Create a budget</Link></div>
-        </div>
-    )
->>>>>>> ebc9727ddbfe9e0563a190ebf986f7c478b4b5e0
 }
 
 export default Welcome
