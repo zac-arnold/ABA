7c4e8d5a46eac47a157f340979ae66f8b658f80b
j-alipate <j.alipate@gmail.com>
2020-07-16 13:18:33 +1200

    welcome page added

--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Welcome = () => {
     return(
         <div id='welcome-page'>
-            <div id='welcome-create-budget-button'>Create a budget</div>
+            <div id='welcome-create-budget-button'><Link to='/bugdet'>Create a budget</Link></div>
         </div>
     )
 }
