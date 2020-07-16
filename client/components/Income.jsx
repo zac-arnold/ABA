37d26c63250714fc7a4f21663a34351ad4b8f661
Ariel <33848430+arielwc@users.noreply.github.com>
2020-07-16 15:39:50 +1200

    create changeHandler function in Income component

--- client/components/Income.jsx
+++ client/components/Income.jsx
@@ -6,16 +6,44 @@ class Income extends React.Component {
     incomes: [{ name: '', amount: 0, occurrance: 'weekly' }]
   }
 
+  changeHandler = (evt, index) => {
+    // const { value, name } = evt.target
+    this.state.incomes[index] = evt.target.value
+    this.setState({
+      incomes: this.state.incomes.index
+    })
+    // console.log(value, name)
+  }
+
+  changeHandlerAmount = (evt, index) => {
+    // const { value, name } = evt.target
+    this.state.incomes[index] = evt.target.value
+    this.setState({
+      incomes: this.state.incomes.amount
+    })
+    // console.log(value, name)
+  }
+
+
   render () {
+    console.log(this.state)
     return (
       <div>
         <form>
-          <label htmlFor="name">Name </label>
-          <input type="text" value="" />
-
-          <label htmlFor="amount">Amount </label>
-          <input type="text" value="" />
-          <button type="submit" value="submit">Submit</button>
+          {
+            this.state.incomes.map((name, index) => {
+              console.log(name.name)
+              return (
+                <div key={index}>
+                  <label htmlFor="name">Name </label>
+                  <input type="text" value={name.name} placeholder="Income Type" name="name" onChange={evt => this.changeHandler(evt, index)} />
+                  <label htmlFor="amount">Amount </label>
+                  <input type="text" value={name.amount} placeholder="Amount" name="amount" onChange={evt => this.changeHandlerAmount(evt, index)}/>
+                  <button type="submit" value="submit">Submit</button>
+                </div>
+              )
+            })
+          }
         </form>
       </div>
     )
