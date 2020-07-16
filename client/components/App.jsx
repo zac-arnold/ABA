4b7ddc684554e9eef04bd6bbd27c9009050f8823
j-alipate <j.alipate@gmail.com>
2020-07-16 13:04:41 +1200

    installed semantic-ui

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import { Container } from 'semantic-ui-react'
+import Welcome from './Welcome'
 
 const App = () => (
 
   <Router>
     <Container style={{ marginTop: 75 }}>
       <Switch>
+      <Route exact path='/' component={Welcome} />
         {/* <Route exact path='/' component={FoodList} />
         <Route exact path='/foods/:id' component={FoodDetails}/> */}
       </Switch>
---
+++ client/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Welcome = () => {
+    return(
+        <div id='welcome-page'>
+            <div id='welcome-create-budget-button'>Create a budget</div>
+        </div>
+    )
+}
+
+export default Welcome
