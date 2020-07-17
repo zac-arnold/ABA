8c57b6d5bae2685bc6e5fd98cce1e32eb0ac83e4
petemcnie <petemcnie0@gmail.com>
2020-07-18 11:10:19 +1200

    Register adding users to db

--- server/db/clients.js
+++ server/db/clients.js
@@ -1,20 +1,34 @@
 const connection = require('./connection')
+const { hash, generateSalt } = require('../../support/crypto')
 
 module.exports = {
-  createClient
+  registerUser
 }
 
-function createClient (credentials, db = connection) {
+function registerUser (credentials, db = connection) {
   console.log('client.js ', credentials)
   return doesUserExist(credentials.username, db)
-   .then(exists => {
-     if (exists) {
-       return Promise.reject(new Error('User exists'))
-     }
-   })
-  //  .then(() => )
-
-
+    .then(exists => {
+      if (exists) {
+        return Promise.reject(new Error('User exists'))
+      }
+    })
+    .then(() => generateSalt())
+    .then(async (salt) => {
+      return {
+        passwordHash: await hash(credentials.password, salt),
+        salt
+      }
+    })
+    .then(hashes => {
+      return db('user')
+        .insert({
+          username: credentials.username,
+          email: credentials.email,
+          password: hashes.passwordHash,
+          salt: hashes.salt
+        })
+    })
 }
 
 function doesUserExist (username, db = connection) {
@@ -26,25 +40,3 @@ function doesUserExist (username, db = connection) {
       return rows[0].number > 0
     })
 }
-
-
-//   return db('user')
-//     .where('username', credentials.username)
-//     .then(user => {
-//       console.log('client.js ', user)
-//       // if there is NOT a user, then clearly that user doesn't
-//       // exist, and we should just return out of the login() function
-//       // (probably return null to tell the caller "login didn't work")
-
-//       // if there IS a user, then we need to check:
-//       // does credentials.password, when hashed/salted, equal user.password?
-//       // you'll need to use the same scrypt stuff we did in the seed file,
-//       // something like:
-//       // crypto.scrypt(credentials.password, user.salt, 64 .....)
-//       // and then with the derivedKey, check if it === user.password
-//       //
-//       // if it does match, then probably return the user record out of
-//       // this function, so that the caller knows "yes the user did log in"
-//     })
-//     .catch(err => console.log(err))
-// }
--- server/routes/register.js
+++ server/routes/register.js
@@ -10,5 +10,13 @@ router.post('/', async (req, res) => {
   console.log('register.js: ', req.body)
   const { username, email, password } = req.body
   const credentials = { username, email, password }
-  return db.createClient(credentials)
+  return db.registerUser(credentials)
+    .then(() => {
+      return db.login(username, password)
+    })
+    .catch(err => {
+      return res.status(400).send(err.message)
+    })
 })
+
+//router.post('/login', .... db.login())
