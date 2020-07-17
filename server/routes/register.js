0f8a76dba329f1a6e3390a3652ba441c8d4c79ae
Pete McNie <62854343+PeteMcNie@users.noreply.github.com>
2020-07-18 08:32:09 +1200

    Add route and db files

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,9 +1,14 @@
 const express = require('express')
 
+const db = require('../db/clients')
+
 const router = express.Router()
 
 module.exports = router
 
 router.post('/', async (req, res) => {
   console.log('register.js: ', req.body)
+  const { username, email, password } = req.body
+  const credentials = { username, email, password }
+  return db.createClient(credentials)
 })
