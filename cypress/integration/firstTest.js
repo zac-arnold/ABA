5cf2b602fa2cda45d0b5513fe47b895ad56a86da
Zac Arnold <63273003+zac-arnold@users.noreply.github.com>
2020-07-17 14:56:35 +1200

    debug cypress

---
+++ cypress/integration/firstTest.js
@@ -0,0 +1,5 @@
+describe('the Welcome component', () => {
+  it('Visits Local Host 3000', () => {
+    cy.visit('http://localhost:3000/')
+  })
+})
--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -1,37 +1,35 @@
-/* global cy */
+// describe('Welcome Component', () => {
+// beforeEach(() => {
+//   Add a reset and seed the database prior to every test
+//   Without this, the food list will not show on the screen
+//   cy.exec('npm run db:reset && npm run db:seed')
+// })
 
-describe('The Home Page', () => {
-  // beforeEach(() => {
-  //   Add a reset and seed the database prior to every test
-  //   Without this, the food list will not show on the screen
-  //   cy.exec('npm run db:reset && npm run db:seed')
-  // })
+// it('Successfully loads the welcome page', () => {
+//   cy.visit('/')
+//   cy.get('h1')
+//     .contains('A Budgetting App')
+// })
 
-  it('Successfully loads the home page', () => {
-    cy.visit('/')
-    cy.get('h1')
-      .contains('A Budgetting App')
+//   cy.get('.subtitle')
+//     .contains('Home')
+// })
 
-    //   cy.get('.subtitle')
-    //     .contains('Home')
-    // })
+// it('Correct number of foods', () => {
+//   cy.get('.food')
+//     .its('length')
+//     .should('be', 27)
+// })
 
-    // it('Correct number of foods', () => {
-    //   cy.get('.food')
-    //     .its('length')
-    //     .should('be', 27)
-    // })
+// it('Drop down list contains items', () => {
+//   cy.get('#category-list')
+//     .contains('Fruits')
+// })
 
-    // it('Drop down list contains items', () => {
-    //   cy.get('#category-list')
-    //     .contains('Fruits')
-    // })
-
-  // it('Can navigate to the food details page', () => {
-  //   cy.server()
-  //   // alias the network request
-  //   cy.route('/200?**').as('networkRequest')
-  //   cy.get('.food').first().click()
-  //   cy.url().should('include', '/foods/1')
-  })
-})
+// it('Can navigate to the food details page', () => {
+//   cy.server()
+//   // alias the network request
+//   cy.route('/200?**').as('networkRequest')
+//   cy.get('.food').first().click()
+//   cy.url().should('include', '/foods/1')
+// })
