0d509bd21884d23fa94cc85da7aac91eb22b2edd
Pete McNie <62854343+PeteMcNie@users.noreply.github.com>
2020-07-19 11:49:26 +1200

    new client added to store

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 
 import income from './income'
 import expense from './expense'
+import newClient from './newClient'
 
 export default combineReducers({
   income,
-  expense
+  expense,
+  newClient
 })
