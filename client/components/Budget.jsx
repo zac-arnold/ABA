f1606e42e7813b23cb1b96452a61de1cadd42af7
j-alipate <j.alipate@gmail.com>
2020-07-18 18:21:26 +1200

    added nav bar

--- client/components/Budget.jsx
+++ client/components/Budget.jsx
@@ -1,18 +1,57 @@
 import React from 'react'
-
+import { Container, Row, Col, Carousel } from 'react-bootstrap'
 import Title from './Title'
 import SideBar from './SideBar'
-import GraphSlider from './GraphSlider'
 
 const Budget = () => {
   return (
-    <>
-      <Title />
-      <div id='budget-wrapper'>
-      <SideBar />
-      <GraphSlider />
-      </div>
-    </>
+    <Container fluid className='p-0'>
+        <Title />
+      <Row>
+        <Col>
+          <SideBar />
+        </Col>
+        <Col>
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://via.placeholder.com/400?text=First slide"
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://via.placeholder.com/400?text=Second slide"
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://via.placeholder.com/400?text=Third slide"
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
--- client/components/Title.jsx
+++ client/components/Title.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Navbar, FormControl, Nav, Form, Button} from 'react-bootstrap'
 
 const Title = () => {
   return (
-    <div className='title-bar'>
-      <h3>BALANCE</h3>
-    </div>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#features">Features</Nav.Link>
+        <Nav.Link href="#pricing">Pricing</Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-info">Search</Button>
+      </Form>
+    </Navbar>
   )
 }
 
