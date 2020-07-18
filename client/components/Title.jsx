403da04b41c52c6aa5a9a6e1ac32ee7384deda53
Zac Arnold <63273003+zac-arnold@users.noreply.github.com>
2020-07-19 10:20:13 +1200

    create darkmode

--- client/components/Title.jsx
+++ client/components/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Title = () => {
   return (
-    <div className='title-bar'>
+    <div className='title-bar' id="dark-change">
       <h3>BALANCE</h3>
     </div>
   )
