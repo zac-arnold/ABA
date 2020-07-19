647369db97abda1a014e912b7cb66af32cfd782e
Zac Arnold <videozac@gmail.com>
2020-07-19 18:56:18 +1200

    create more tests and add id to income reducer

--- client/reducers/income.test.js
+++ client/reducers/income.test.js
@@ -3,7 +3,7 @@ import { SEND_INCOME, DELETE_INCOME } from '../actions'
 
 test('SEND_INCOME adds an income to the Income array', () => {
   const testInitialState = [{
-
+    id: 300,
     name: 'My Salary',
     amount: 50000,
     frequency: 'annually'
@@ -13,6 +13,7 @@ test('SEND_INCOME adds an income to the Income array', () => {
   const testAction = {
     type: SEND_INCOME,
     income: {
+      id: 400,
       name: 'Secondary Salary',
       amount: 1000000,
       frequency: 'one off'
@@ -23,32 +24,37 @@ test('SEND_INCOME adds an income to the Income array', () => {
 
   expect(newState).toHaveLength(2)
   expect(newState[1].name).toBe('Secondary Salary')
+  expect(newState[1].id).toBe(400)
   expect(newState[1].amount).toBe(1000000)
 })
 
-test('DELETE_INCOME deletes an income from the Income array'() => {
+test('DELETE_INCOME deletes an income from the Income array', () => {
   // Arrange
   const testInitialState = [{
+    id: 200,
     name: 'My Salary',
     amount: 50000,
     frequency: 'annually'
   },
   {
-    name: 'My Salary',
-    amount: 50000,
-    frequency: 'annually'
+    id: 300,
+    name: '2nd Salary',
+    amount: 20,
+    frequency: 'one off'
   }]
 
   const testAction = {
-    type: SEND_INCOME,
+    type: DELETE_INCOME,
     income: {
-      name: 'Secondary Salary',
-      amount: 1000000,
+      id: 300,
+      name: '2nd Salary',
+      amount: 20,
       frequency: 'one off'
     }
   }
   // Act
   const newState = incomes(testInitialState, testAction)
 
-  // 
+  // Assert
+  expect(newState).toHaveLength(1)
 })
