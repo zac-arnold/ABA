27a5eb828c0b0f6ffef0b579f9c124c5c7c4e2fe
Zac Arnold <videozac@gmail.com>
2020-07-19 18:41:25 +1200

    income test

--- client/reducers/income.test.js
+++ client/reducers/income.test.js
@@ -25,3 +25,30 @@ test('SEND_INCOME adds an income to the Income array', () => {
   expect(newState[1].name).toBe('Secondary Salary')
   expect(newState[1].amount).toBe(1000000)
 })
+
+test('DELETE_INCOME deletes an income from the Income array'() => {
+  // Arrange
+  const testInitialState = [{
+    name: 'My Salary',
+    amount: 50000,
+    frequency: 'annually'
+  },
+  {
+    name: 'My Salary',
+    amount: 50000,
+    frequency: 'annually'
+  }]
+
+  const testAction = {
+    type: SEND_INCOME,
+    income: {
+      name: 'Secondary Salary',
+      amount: 1000000,
+      frequency: 'one off'
+    }
+  }
+  // Act
+  const newState = incomes(testInitialState, testAction)
+
+  // 
+})
