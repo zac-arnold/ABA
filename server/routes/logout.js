13802385b7e4adf3b7aead88cbf25f184b31662d
petemcnie <petemcnie0@gmail.com>
2020-07-20 07:27:17 +1200

    Tidied up linting

--- server/routes/logout.js
+++ server/routes/logout.js
@@ -10,10 +10,9 @@ router.get('/', (req, res) => {
   console.log('logout route ', req.cookies)
   const sessionId = req.cookies
   console.log('logout.js', sessionId)
-//   db.deleteSession(sessionId)
-// NEED TO FIND A WAY TO DELETE SESSION FROM DATABASE, CANNOT CHANGE THIS TO A 
-// DELETE ROUTE WITHOUT BREAKING CLIENT_SIDE DELETION
-  
+  //   db.deleteSession(sessionId)
+  // NEED TO FIND A WAY TO DELETE SESSION FROM DATABASE, CANNOT CHANGE THIS TO A 
+  // DELETE ROUTE WITHOUT BREAKING CLIENT_SIDE DELETION
   res.clearCookie('session', { expires: new Date(0) })
   res.redirect('/')
 })
