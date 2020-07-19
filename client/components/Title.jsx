9a40ec83c869aa6e461eb7218dbbb5a99cd948c1
Zac Arnold <63273003+zac-arnold@users.noreply.github.com>
2020-07-19 14:47:50 +1200

    picked color-scheme for home page

--- client/components/Title.jsx
+++ client/components/Title.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { Navbar, FormControl, Nav, Form, Button } from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
 
 const Title = () => {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="light" variant="light">
       <Navbar.Brand href="#home">Navbar</Navbar.Brand>
       <Nav className="mr-auto">
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#features">Features</Nav.Link>
         <Nav.Link href="#pricing">Pricing</Nav.Link>
+
       </Nav>
 
     </Navbar>
