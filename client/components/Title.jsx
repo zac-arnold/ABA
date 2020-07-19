97145d58a10f4dd3e76d3664a2121b607d2395d1
Zac Arnold <63273003+zac-arnold@users.noreply.github.com>
2020-07-19 17:30:24 +1200

    added sign up button, played with margins and padding

--- client/components/Title.jsx
+++ client/components/Title.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, FormControl, Nav, Form, Button } from 'react-bootstrap'
 
 const Title = () => {
   return (
     <Navbar bg="light" variant="light">
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Navbar.Brand href="#home">Balance</Navbar.Brand>
       <Nav className="mr-auto">
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#features">Features</Nav.Link>
         <Nav.Link href="#pricing">Pricing</Nav.Link>
-
       </Nav>
+      <Button variant="secondary" size="lg mr-2">Sign-up</Button>
+      <Button variant="secondary" size="lg">Login</Button>
 
     </Navbar>
   )
