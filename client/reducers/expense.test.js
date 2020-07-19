9c8960a9f2594c142c2264355fd33b2f42373cab
Pete McNie <62854343+PeteMcNie@users.noreply.github.com>
2020-07-19 15:44:15 +1200

    add Delete test

--- client/reducers/expense.test.js
+++ client/reducers/expense.test.js
@@ -1,8 +1,7 @@
 import expenses from './expense'
 import { SEND_EXPENSE, DELETE_EXPENSE } from '../actions'
 
-test ('SEND_EXPENSE adds expense to expense array',() => {
-
+test('SEND_EXPENSE adds expense to expense array', () => {
   const initialTestState = [{
     id: 200,
     name: 'Power bill',
@@ -14,7 +13,7 @@ test ('SEND_EXPENSE adds expense to expense array',() => {
 
   const testAction = {
     type: SEND_EXPENSE,
-    expense: { 
+    expense: {
       id: 666420,
       name: 'Food',
       amount: '150',
@@ -25,6 +24,34 @@ test ('SEND_EXPENSE adds expense to expense array',() => {
   const newState = expenses(initialTestState, testAction)
 
   expect(newState).toHaveLength(2)
-  expect(newState[1].amount).toBe('150')
+  expect(newState[1].amount).toBe(150)
 })
 
+test('DELETE_EXPENSE deletes expense from expense array', () => {
+  // Arrange
+  const initialTestState = [{
+    id: 200,
+    name: 'Power bill',
+    date: 1594938869,
+    amount: 300,
+    frequency: 'weekly',
+    category: 'Utilities'
+  }]
+
+  const testAction = {
+    type: DELETE_EXPENSE,
+    expense: {
+      id: 666420,
+      name: 'Food',
+      amount: '150',
+      category: 'Groceries',
+      frequency: 'One off'
+    }
+  }
+
+  // Act
+  const newState = expenses(initialTestState, testAction)
+
+  // Assert
+  expect(newState).toHaveLength(1)
+})
