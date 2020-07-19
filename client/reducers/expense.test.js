4f2131e56e1fbbf1f71df683b43e97ab671ff64f
Zac Arnold <videozac@gmail.com>
2020-07-19 15:30:08 +1200

    add reducer test

---
+++ client/reducers/expense.test.js
@@ -0,0 +1,30 @@
+import expenses from './expense'
+import { SEND_EXPENSE, DELETE_EXPENSE } from '../actions'
+
+test ('SEND_EXPENSE adds expense to expense array',() => {
+
+  const initialTestState = [{
+    id: 200,
+    name: 'Power bill',
+    date: 1594938869,
+    amount: 300,
+    frequency: 'weekly',
+    category: 'Utilities'
+  }]
+
+  const testAction = {
+    type: SEND_EXPENSE,
+    expense: { 
+      id: 666420,
+      name: 'Food',
+      amount: '150',
+      category: 'Groceries',
+      frequency: 'One off'
+    }
+  }
+  const newState = expenses(initialTestState, testAction)
+
+  expect(newState).toHaveLength(2)
+  expect(newState[1].amount).toBe('150')
+})
+
