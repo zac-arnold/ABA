44bc6e49603c5bab14dd9bc7bb73075b86dcd4f5
Pete McNie <62854343+PeteMcNie@users.noreply.github.com>
2020-07-20 13:53:51 +1200

    RegisterModal connected and sending data

--- client/components/Title.jsx
+++ client/components/Title.jsx
@@ -1,49 +1,74 @@
 import React from 'react'
 import { Navbar, Nav, Button, Modal, Form } from 'react-bootstrap'
 
-import SignIn from './SignIn'
-import Register from './Register'
+// import { register } from '../actions'
+
+import RegisterModal from './RegisterModal'
+// import SignIn from './SignIn'
+// import Register from './Register'
 // import Logout from './Logout'
 
-function RegisterModal (props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Create account
-        </Modal.Title>
-      </Modal.Header>
-      <Form>
-        <Modal.Body>
-          <Form.Group controlId="formBasicUsername">
-            <Form.Label>Username</Form.Label>
-            <Form.Control type="text" placeholder="Choose a username" />
-          </Form.Group>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+// class RegisterModal extends React.Component {
+//   state = {
+//     username: '',
+//     email: '',
+//     password: ''
+//   }
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={props.onHide} type="submit">Register</Button>
-        </Modal.Footer>
-      </Form>
-    </Modal>
-  )
-}
+//   handleChange = evt => {
+//     const { name, value } = evt.target
+//     this.setState({
+//       [name]: value
+//     })
+//   }
+
+//   submitHandler = evt => {
+//     console.log('title.jsx ', this.state)
+//     evt.preventDefault()
+//     this.props.dispatch(register(this.state))
+//   }
+
+
+//   render () {
+//     return (
+//       <Modal
+//         {...this.props}
+//         size="lg"
+//         aria-labelledby="contained-modal-title-vcenter"
+//         centered
+//       >
+//         <Modal.Header closeButton>
+//           <Modal.Title id="contained-modal-title-vcenter">
+//           Create account
+//           </Modal.Title>
+//         </Modal.Header>
+//         <Form onSubmit={(evt) => this.submitHandler(evt)}>
+//           <Modal.Body>
+//             <Form.Group>
+//               <Form.Label>Username</Form.Label>
+//               <Form.Control type="text" placeholder="Choose a username" />
+//             </Form.Group>
+//             <Form.Group>
+//               <Form.Label>Email address</Form.Label>
+//               <Form.Control type="email" placeholder="Enter email" />
+//               <Form.Text className="text-muted">
+//                 We'll never share your email with anyone else.
+//               </Form.Text>
+//             </Form.Group>
+
+//             <Form.Group>
+//               <Form.Label>Password</Form.Label>
+//               <Form.Control type="password" placeholder="Password" />
+//             </Form.Group>
+//           </Modal.Body>
+//           <Modal.Footer>
+//             <Button onClick={this.props.onHide} type="submit">Register</Button>
+//           </Modal.Footer>
+//         </Form>
+//       </Modal>
+//     )
+//   }
+// }
 
 function Title () {
   const [modalShow, setModalShow] = React.useState(false)
@@ -58,13 +83,10 @@ function Title () {
         <Nav.Link href="#pricing">Pricing</Nav.Link> */}
         </Nav>
         <Button variant="secondary" size="lg mr-2" onClick={() => setModalShow(true)}>Register</Button>
-
         {/* <Button variant="secondary" size="lg">Login</Button> */}
 
         {/* <SignIn /> */}
-        {/* <Register /> */}
         {/* <Logout /> */}
-
       </Navbar>
       <RegisterModal show={modalShow} onHide={() => setModalShow(false)}/>
     </>
