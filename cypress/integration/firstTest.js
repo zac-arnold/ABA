cb16fc5a20700df34d656b4779fecdb5ba49f3c5
Zac Arnold <videozac@gmail.com>
2020-07-21 11:55:15 +1200

    add extra to E2E test

--- cypress/integration/firstTest.js
+++ cypress/integration/firstTest.js
@@ -1,11 +1,7 @@
+/* global cy */
+
 describe('the Welcome component', () => {
   it('Visits Local Host 3000', () => {
     cy.visit('http://localhost:3000/')
-  })
-})
-
-describe('the budget page', () => {
-  it('visits the budget page', () => {
-    cy.vist('http://localhost:3000/budget')
   })
 })
