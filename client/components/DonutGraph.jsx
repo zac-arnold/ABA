d7b139b016a0e7c0dfc3cf8e9a3720a03291f6b6
Johnny <4018196+j-alipate@users.noreply.github.com>
2020-07-21 10:32:07 +1200

    fixed frequency bug

--- client/components/DonutGraph.jsx
+++ client/components/DonutGraph.jsx
@@ -21,7 +21,7 @@ class DonutGraph extends React.Component {
       const { data, totalExpenses } = this.updateData(this.props)
       let x = 0
       const textAnimation = () => {
-        if (x++ <= 40) {
+        if (x++ <= 50) {
           d3.selectAll('svg > *').remove()
           this.updateGraph(data, 0.8, '$' + (totalExpenses * (x / 50)).toFixed(2), 'your balance')
           setTimeout(textAnimation, 10)
@@ -29,7 +29,7 @@ class DonutGraph extends React.Component {
       }
       textAnimation()
     } else if (!(this.props.incomes.length > 0) && this.props.expenses.length > 0) { // case expense only filled out
-      const totalExpenses = sumOfAmounts(this.props.expenses)
+      const totalExpenses = sumOfAmounts(this.props.expenses) * (30.4375 / this.props.expenses[0].frequency)
       let x = 0
       const textAnimation = () => {
         if (x++ <= 50) {
@@ -40,7 +40,7 @@ class DonutGraph extends React.Component {
       }
       textAnimation()
     } else if (this.props.incomes.length > 0 && !(this.props.expenses.length > 0)) { // case input only filled out
-      const totalIncomes = sumOfAmounts(this.props.incomes)
+      const totalIncomes = sumOfAmounts(this.props.incomes) * (30.4375 / this.props.incomes[0].frequency)
       let x = 0
       const textAnimation = () => {
         if (x++ <= 50) {
