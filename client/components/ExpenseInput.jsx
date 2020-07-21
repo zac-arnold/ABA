9ef3e7414dd06730f48e811e421356626385b213
Ariel <33848430+arielwc@users.noreply.github.com>
2020-07-22 11:34:36 +1200

    changed focus box-shadow placeholder text color for contrast

--- client/components/ExpenseInput.jsx
+++ client/components/ExpenseInput.jsx
@@ -49,7 +49,7 @@ class ExpenseInput extends React.Component {
             <Form.Row className='m-0 p-2'>
 
               <Col>
-                <OverlayTrigger data-trigger="hover focus" placement="bottom"
+                <OverlayTrigger data-trigger="hover focus" placement="top"
                   overlay={<Popover id="popover-basic">
                     <Popover.Title as="h3">Expense Description</Popover.Title>
                     <Popover.Content>Please enter an expense <strong>description</strong> e.g rent</Popover.Content>
@@ -58,7 +58,7 @@ class ExpenseInput extends React.Component {
                 </OverlayTrigger>
               </Col>
               <Col>
-                <OverlayTrigger data-trigger="hover focus" placement="bottom"
+                <OverlayTrigger data-trigger="hover focus" placement="top"
                   overlay={<Popover id="popover-basic">
                     <Popover.Title as="h3">Expense Amount</Popover.Title>
                     <Popover.Content>Please enter an expense <strong>amount</strong></Popover.Content>
@@ -67,7 +67,7 @@ class ExpenseInput extends React.Component {
                 </OverlayTrigger>
               </Col>
               <Col>
-                <OverlayTrigger data-trigger="hover focus" placement="bottom"
+                <OverlayTrigger data-trigger="hover focus" placement="top"
                   overlay={<Popover id="popover-basic">
                     <Popover.Title as="h3">Expense Category</Popover.Title>
                     <Popover.Content>Please choose an expense <strong>category</strong></Popover.Content>
@@ -85,12 +85,12 @@ class ExpenseInput extends React.Component {
                 </OverlayTrigger>
               </Col>
               <Col>
-                <OverlayTrigger data-trigger="hover focus" placement="bottom"
+                <OverlayTrigger data-trigger="hover focus" placement="top"
                   overlay={<Popover id="popover-basic">
                     <Popover.Title as="h3">Expense Frequency</Popover.Title>
                     <Popover.Content>Please choose an expense <strong>frequency</strong></Popover.Content>
                   </Popover>}>
-                  <Form.Control required name='frequency' value={this.state.frequency} onChange={(evt) => this.changeHandler(evt)} size='sm' as="select">
+                  <Form.Control required name='frequency' value={this.state.frequency} onChange={(evt) => this.changeHandler(evt)} aria-label="frequency" size='sm' as="select">
                     <option value='1'>One-off</option>
                     <option value='7'>Weekly</option>
                     <option value='14'>Fortnightly</option>
