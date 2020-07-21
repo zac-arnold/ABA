b34223f9b31a830efe39bfc2a8c8de33ef6d755c
Zac Arnold <63273003+zac-arnold@users.noreply.github.com>
2020-07-21 16:56:43 +1200

    create migrations

---
+++ migrations/20200721165514_expense.js
@@ -0,0 +1,13 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('expense', table => {
+    table.increments('id').primary()
+    table.string('amount')
+    table.string('description')
+    table.string('category')
+    table.string('frequency')
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('expense')
+}
