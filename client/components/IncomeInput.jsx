2bdb9120720a896e93d71c8f907aef62209abb46
Johnny <4018196+j-alipate@users.noreply.github.com>
2020-07-21 15:26:25 +1200

    pre-merge

--- client/components/IncomeInput.jsx
+++ client/components/IncomeInput.jsx
@@ -20,87 +20,80 @@ class IncomeInput extends React.Component {
     })
   }
 
-    sendToStore = (e) => {
-      e.preventDefault()
-      if (!e.target.checkValidity()) return
-      this.state.id = Date.now()
-      this.setState({
-        amount: this.state.amount,
-        description: this.state.description,
-        category: this.state.category,
-        frequency: this.state.frequency
-      })
-      this.props.dispatch(sendIncomeToStore(this.state))
-      this.setState({
-        id: 0,
-        amount: '',
-        description: '',
-        category: '',
-        frequency: 1
-      })
-    }
+  sendToStore = (e) => {
+    const data = this.state
+    data.id = Date.now()
+    this.props.dispatch(sendIncomeToStore(this.state))
+    this.setState({
+      id: 0,
+      amount: '',
+      description: '',
+      category: '',
+      frequency: 1
+    })
+  }
 
-    render () {
-      return (
+  render() {
+    return (
 
-        <Form onSubmit={this.sendToStore}>
-          <Container className='mb-2 border border-dark rounded p-0 bg-dark text-white'>
-            <Row className='align-middle pt-2 m-0'>
-              <Col className='float-left'>
-                            Income
-              </Col>
-              <Col>
-                <Button className='float-right' size='sm' type='submit'>Add</Button>
-              </Col>
-            </Row>
-            <Form.Row className='m-0 p-2'>
-              <Col>
-                <OverlayTrigger data-trigger="hover focus" placement="bottom"
-                  overlay={<Popover id="popover-basic">
-                    <Popover.Title as="h3">Income Amount</Popover.Title>
-                    <Popover.Content>Please enter an income <strong>amount</strong></Popover.Content>
-                  </Popover>}>
-                  <FormControl name='amount' value={this.state.amount} onChange={(evt) => this.changeHandler(evt)} size='sm' aria-label="Amount" placeholder='$' />
-                </OverlayTrigger>
-              </Col>
-              <Col>
-                <OverlayTrigger data-trigger="hover focus" placement="bottom"
-                  overlay={<Popover id="popover-basic">
-                    <Popover.Title as="h3">Income Description</Popover.Title>
-                    <Popover.Content>Please enter an income <strong>description</strong> e.g Salary</Popover.Content>
-                  </Popover>}>
-                  <FormControl name='description' value={this.state.description} onChange={(evt) => this.changeHandler(evt)} size='sm' aria-label="Description" placeholder='Description' />
-                </OverlayTrigger>
-              </Col>
-              <Col>
-                <OverlayTrigger data-trigger="hover focus" placement="bottom"
-                  overlay={<Popover id="popover-basic">
-                    <Popover.Title as="h3">Category</Popover.Title>
-                    <Popover.Content>Please enter an income <strong>category</strong></Popover.Content>
-                  </Popover>} >
-                  <FormControl name='category' value={this.state.category} onChange={(evt) => this.changeHandler(evt)} size='sm' aria-label="Category" placeholder='Category' />
-                </OverlayTrigger>
-              </Col>
-              <Col>
-                <OverlayTrigger data-trigger="hover focus" placement="bottom"
-                  overlay={<Popover id="popover-basic">
-                    <Popover.Title as="h3">Income Frequency</Popover.Title>
-                    <Popover.Content>Please choose an income <strong>frequency</strong></Popover.Content>
-                  </Popover>} >
-                  <Form.Control name='frequency' value={this.state.frequency} onChange={(evt) => this.changeHandler(evt)} size='sm' as="select">
-                    <option value='1'>One-off</option>
-                    <option value='7'>Weekly</option>
-                    <option value='14'>Fortnightly</option>
-                    <option value='30.4375'>Monthly</option>
-                    <option value='365.25'>Yearly</option>
-                  </Form.Control >
-                </OverlayTrigger>
-              </Col>
-            </Form.Row>
-          </Container>
-        </Form>
-      )
-    }
+      <Form onSubmit={this.sendToStore}>
+        <Container className='mb-2 border border-dark rounded p-0 bg-dark text-white'>
+          <Row className='align-middle pt-2 m-0'>
+            <Col className='float-left'>
+              Income
+            </Col>
+            <Col>
+              <Button onClick={() => this.sendToStore()} className='float-right' size='sm'>Add</Button>
+            </Col>
+          </Row>
+          <Form.Row className='m-0 p-2'>
+            <Col>
+              <OverlayTrigger data-trigger="hover focus" placement="bottom"
+                overlay={<Popover id="popover-basic">
+                  <Popover.Title as="h3">Income Amount</Popover.Title>
+                  <Popover.Content>Please enter an income <strong>amount</strong></Popover.Content>
+                </Popover>}>
+                <FormControl name='amount' value={this.state.amount} onChange={(evt) => this.changeHandler(evt)} size='sm' aria-label="Amount" placeholder='$' />
+              </OverlayTrigger>
+            </Col>
+            <Col>
+              <OverlayTrigger data-trigger="hover focus" placement="bottom"
+                overlay={<Popover id="popover-basic">
+                  <Popover.Title as="h3">Income Description</Popover.Title>
+                  <Popover.Content>Please enter an income <strong>description</strong> e.g Salary</Popover.Content>
+                </Popover>}>
+                <FormControl name='description' value={this.state.description} onChange={(evt) => this.changeHandler(evt)} size='sm' aria-label="Description" placeholder='Description' />
+              </OverlayTrigger>
+            </Col>
+            <Col>
+              <OverlayTrigger data-trigger="hover focus" placement="bottom"
+                overlay={<Popover id="popover-basic">
+                  <Popover.Title as="h3">Category</Popover.Title>
+                  <Popover.Content>Please enter an income <strong>category</strong></Popover.Content>
+                </Popover>} >
+                <FormControl name='category' value={this.state.category} onChange={(evt) => this.changeHandler(evt)} size='sm' aria-label="Category" placeholder='Category' />
+              </OverlayTrigger>
+            </Col>
+            <Col>
+              <OverlayTrigger data-trigger="hover focus" placement="bottom"
+                overlay={<Popover id="popover-basic">
+                  <Popover.Title as="h3">Income Frequency</Popover.Title>
+                  <Popover.Content>Please choose an income <strong>frequency</strong></Popover.Content>
+                </Popover>} >
+                <Form.Control name='frequency' value={this.state.frequency} onChange={(evt) => this.changeHandler(evt)} size='sm' as="select">
+                  <option value='1'>One-off</option>
+                  <option value='7'>Weekly</option>
+                  <option value='14'>Fortnightly</option>
+                  <option value='30.4375'>Monthly</option>
+                  <option value='365.25'>Yearly</option>
+                </Form.Control >
+              </OverlayTrigger>
+            </Col>
+          </Form.Row>
+        </Container>
+      </Form>
+    )
+  }
 }
 
 export default connect()(IncomeInput)
