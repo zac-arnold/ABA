b16029757ad21c394a8b191197a17f5972322e05
j-alipate <j.alipate@gmail.com>
2020-07-22 05:06:12 +1200

    removed p tags

--- client/components/Budget.jsx
+++ client/components/Budget.jsx
@@ -9,7 +9,7 @@ class Budget extends React.Component {
     return (
       <Row className='m-0'>
         <Col className='p-0 md-12' md={12} lg={6}>
-          <p className='text-center mt-4 strong'><h3>Your Monthly Summary</h3></p>
+          <h3 className='text-center mt-4 strong'>Your Monthly Summary</h3>
           <Row>
             <div className='p-1 my_dataviz overflow-hidden mt-4 m-auto align-middle'>
               <svg id="my_dataviz"></svg>
